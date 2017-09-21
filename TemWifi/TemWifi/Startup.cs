d80afd6e5dca35f30db29ccdd95a7e24e6ff8eda
Renan Barbosa <rnn2@live.com>
2017-09-20 23:36:46 -0300

    Criação do Projeto Web

---
+++ TemWifi/TemWifi/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TemWifi.Startup))]
+namespace TemWifi
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
